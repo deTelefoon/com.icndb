5a42989da054a5eb7844943f98a928e5a2797b88
Emile Nijssen <emilenijssen@gmail.com>
2015-09-08 21:55:55 +0200

    fixes

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 "use strict";
   
-function App() 
-{
+var self = module.exports = {
 	
-}
-
-module.exports = App;
-
-App.prototype.init = function(){
+	init: function(){
 	
-	Homey.manager('speech-input').on('speech', function(speech){
-		
-		require('request').get({
-			url: 'http://api.icndb.com/jokes/random',
-			json: true
-		}, function(err, result, body){
-			if( err ) return false;
-			Homey.manager('speech-output').say( body.value.joke );
+		Homey.manager('speech-input').on('speech', function(speech){
+						
+			require('request').get({
+				url: 'http://api.icndb.com/jokes/random',
+				json: true
+			}, function(err, result, body){
+				if( err ) return Homey.manager('speech-output').say( __('internet_error') );
+				Homey.manager('speech-output').say( body.value.joke );
+			});
+			
 		});
 		
-	});
-	
+	}
 }
