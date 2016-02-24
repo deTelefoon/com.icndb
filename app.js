14b470278d60da405020d5493cc155f37dbf8142
Dennie de Groot <mail@denniedegroot.nl>
2016-02-24 22:31:08 +0100

    add action flow

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 "use strict";
 
 var http = require('http');
-  
-var self = module.exports = {
-	
-	init: function(){
-	
-		Homey.manager('speech-input').on('speech', function(speech){
-						
-			http.get('http://api.icndb.com/jokes/random', function(res){
-	
-				var body = '';
-				
-				res
-					.on('data', function(chunk) {
-						body += chunk;
-					})
-					.on('end', function() {
-						body = JSON.parse(body);
-						Homey.manager('speech-output').say( body.value.joke );
-					});
-					
-			}).on('error', function(e) {
-				console.log("Got error: " + e.message);
-				Homey.manager('speech-output').say( __('internet_error') );
-			});
-			
+
+module.exports.init = function() {
+	Homey.manager('speech-input').on('speech', function(speech){
+		tell_joke();
+	});
+}
+
+Homey.manager('flow').on('action.joke', function(callback, args){
+	tell_joke();
+	callback(null, true);
+});
+
+function tell_joke() {
+	http.get('http://api.icndb.com/jokes/random', function(res){
+		var body = '';
+
+		res.on('data', function(chunk) {
+			body += chunk;
+		}).on('end', function() {
+			body = JSON.parse(body);
+			Homey.manager('speech-output').say(body.value.joke);
 		});
-		
-	}
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+		Homey.manager('speech-output').say( __('internet_error') );
+	});
 }
